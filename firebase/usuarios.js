2db87cdd6c0d9bced4cb9304459dcc6c463c693b
Luan de Almeida da Silva <luanalmeida3976@gmail.com>
2021-05-12 14:19:59 -0300

    3

--- firebase/usuarios.js
+++ firebase/usuarios.js
@@ -1,62 +1,132 @@
 var db = firebase.firestore();
 
-
-let auth = firebase.auth()
-let newUserEmail = "novoteste@teste.com"
-let newUserPassword = "123abc"
-
-
-function newUser() {
-    let newUserEmail = "novoteste@teste.com"
-    let newUserPassword = "123abc"
-
-    let auth = firebase.auth()
-    auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
-        // Retorna uma Promisse
-        .then(user => {
-            console.log(user)
-        }).catch(error => {
-            console.log(error)
-        })
-
-}
-
-
-//Logando Usuario
-function login() {
-    let UserEmail = "novoteste@teste.com"
-    let UserPassword = "123abc"
-
-    auth.setPersistence(firebase.auth.Auth.persistence.SESSION).then(() => {
-
-        auth.signInWithEmailAndPassword(UserEmail, UserPassword)
-            .then(loggedUser => {
-                console.log(auth.currentUser);
-            }).catch(error => {
-                console.log(error)
-            })
-    }).catch(error => {
-        console.log(error)
+function ler (){
+db.collection("Clientes").get().then(snapshot =>{
+    snapshot.forEach(item => {
+        console.log(item.data())
     })
-}
-// login()
-
-
-// Indentificar Usuario que está Logado
-
-auth.onAuthStateChanged(user => {
-    if (user) {
-        console.log(user)
-    } else {
-        console.log(" Ninguém logado")
-    }
+}).catch(error =>{
+    console.log(error)
 })
+}
 
-function logout() {
-    auth.signOut().then(() => {
-        console.log("Usuario Deslogado")
-    }).catch(error => {
+function escrever (){
+    db.collection("Clientes").add({title:"Novo Objeto", numero: Math.random()})
+    .then(doc =>{
+        console.log(doc)
+    })
+    .catch(error =>{
         console.log(error)
     })
 }
-setTimeout(login, 2000)
+
+escrever()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let auth = firebase.auth()
+// let newUserEmail = "novoteste@teste.com"
+// let newUserPassword = "123abc"
+
+
+// function newUser() {
+//     let newUserEmail = "novoteste@teste.com"
+//     let newUserPassword = "123abc"
+
+//     let auth = firebase.auth()
+//     auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
+//         // Retorna uma Promisse
+//         .then(user => {
+//             console.log(user)
+//         }).catch(error => {
+//             console.log(error)
+//         })
+
+// }
+
+
+// //Logando Usuario
+// function login() {
+//     let UserEmail = "novoteste@teste.com"
+//     let UserPassword = "123abc"
+
+//     auth.setPersistence(firebase.auth.Auth.persistence.SESSION).then(() => {
+
+//         auth.signInWithEmailAndPassword(UserEmail, UserPassword)
+//             .then(loggedUser => {
+//                 console.log(auth.currentUser);
+//             }).catch(error => {
+//                 console.log(error)
+//             })
+//     }).catch(error => {
+//         console.log(error)
+//     })
+// }
+// // login()
+
+
+// // Indentificar Usuario que está Logado
+
+// auth.onAuthStateChanged(user => {
+//     if (user) {
+//         console.log(user)
+//     } else {
+//         console.log(" Ninguém logado")
+//     }
+// })
+
+// function logout() {
+//     auth.signOut().then(() => {
+//         console.log("Usuario Deslogado")
+//     }).catch(error => {
+//         console.log(error)
+//     })
+// }
+// setTimeout(login, 2000)
